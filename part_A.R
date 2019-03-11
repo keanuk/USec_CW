f6f8d115a0af8946b43dc49413eb3c8da6c8d0f8
Keanu Kerr <keanukerr@gmail.com>
2019-03-11 08:19:31 +0000

    minor changes

--- part_A.R
+++ part_A.R
@@ -6,5 +6,12 @@ numSurvey <- read.csv(file="numerics-data(2).csv", header=TRUE, sep=",")
 cleanNum <- numSurvey[numSurvey$sebisLikert.sebisCheck. == 5, ]
 cleanNum <- cleanNum[cleanNum$hackersLikert.folkHCheck. == 2, ]
 
-print(cleanNum$sebisLikert.sebisCheck.)
-print(cleanNum$hackersLikert.folkHCheck.)
+westinNum <- cleanNum[, grepl("westin", names(cleanNum))]
+
+# print(cleanNum$sebisLikert.sebisCheck.)
+# print(cleanNum$hackersLikert.folkHCheck.)
+
+for(line in westinNum) {
+    print("New Line")
+    print(line)
+}
